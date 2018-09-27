1f69c569044051a6e727f66300f3c88f180eee28
hema jha <hema.jha@quifers.com>
2018-09-27 17:21:55 +0530

    till production build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 //Populate table of users via API calls
 getUsers().then(result => {
@@ -7,15 +7,30 @@ getUsers().then(result => {
   result.forEach(user => {
       usersBody+=
         `<tr>
-          <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</td>
+          <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
           <td>${user.id}</td>
-          <td>${user.name}</td>
-          <td>${user.lastname}</td>
+          <td>${user.firstName}</td>
+          <td>${user.lastName}</td>
           <td>${user.email}</td>
         </tr>`
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //Must use array, from to create  a real array from DOM collection
+  //getElementsByClassName onln returns and "array like" objects
+  Array.from(deleteLinks, link => {
+    link.onClick = function(event) {
+      console.log("delete clicked"); //eslint-disable-line no-console
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
 
 
